d1f0d1a56e40247e97a4af4744caa94d60401e87
Serg Minin <everestdon@ukr.net>
2018-10-15 14:18:22 +0300

    it remains to add scoring

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import Field from "./UI/Field";
 import Button from "./UI/Button";
 import ContollPanel from "./UI/ControllPanel";
 import Score from "./UI/Score";
-import { initCells, moveCells, directions } from "./Logic";
+import {
+  initCells,
+  moveCells,
+  directions,
+  removeAndIncreaseCells,
+  populateField
+} from "./Logic";
 
 class App extends Component {
   state = this.getNewState();
@@ -35,13 +41,23 @@ class App extends Component {
     document.removeEventListener("keypress", this.handleKeyPress);
   }
 
-  handleKeyPress = event => {
+  handleKeyPress = async event => {
     console.log(event.keyCode);
     if ([52, 50, 54, 56].includes(event.keyCode))
       this.setState(state => ({
         ...state,
         cells: moveCells(state.cells, this.mapKeyCodeToDirection[event.keyCode])
       }));
+
+    await delay(100);
+    this.setState(state => ({
+      ...state,
+      cells: removeAndIncreaseCells(state.cells)
+    }));
+    this.setState(state => ({
+      ...state,
+      cells: populateField(state.cells)
+    }));
   };
 
   render() {
@@ -57,5 +73,7 @@ class App extends Component {
     );
   }
 }
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 export default App;
